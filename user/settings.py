b1564c074759daf3791b4eab32b3de9db3868ee2
rachit7399 <43109029+rachit7399@users.noreply.github.com>
2021-04-16 11:54:09 +0530

    Update settings.py

--- user/settings.py
+++ user/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'rachit.b@engineerbabu.in')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'Rachitshivani@00')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '**********')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '************')
 EMAIL_PORT = 587
 
